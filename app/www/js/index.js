4dc5a531072d8102442ab47a437bdc5217bae418
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2014-06-22 14:48:22 -0300

    Backbone working with JQM

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,47 +1,77 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        Parse.initialize("Va9dcafjWMkGn0PYMK7azpOsHANRD3Nk9AO9xq2e", "49bOHrbFxuQK6RZKB5QLgzuiBDKFYsUrqPiHmmQt");
-        this.bindEvents();
+BasicView = Backbone.View.extend({
+    initialize : function () {
+        //_.bindAll();
+        this.render();
     },
 
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        $('#signup-form').submit(this.onSignUp);
-        $('#signin-form').submit(this.onSignIn);
-        route('home', this.onHome);
+    events : {
+        "click #backButton" : "goBackInHistory"
     },
 
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    role : "page",
+
+    attributes : function () {
+        return {
+            "data-role" : this.role
+        };
     },
 
-    onSignIn: function (event) {
-        var username = $('#signin-username').val()
-        var password = $('#signin-password').val()
+    getHeaderTitle : function () {
+        return this.getSpecificTemplateValues().headerTitle;
+    },
 
-        Parse.User.logIn(username, password, {
-            success: function(user) {
-                // Do stuff after successful login.
-                window.location.href = '#home';
-            },
-            error: function(user, error) {
-                // The login failed. Check error to see why.
-                console.log("Error: " + error.code + " " + error.message);
-            }
+    goBackInHistory : function(clickEvent){
+        history.go(-1);
+        return false;
+    },
+
+    getTemplateResult : function (templateDefinitionID, templateValues) {
+        return _.template($("#" + templateDefinitionID).html())(templateValues)
+    },
+
+    render : function () {
+        //this.cleanupPossiblePageDuplicationInDOM();
+        $(this.el).html(this.getTemplateResult(this.templateId,
+            this.getSpecificTemplateValues()));
+        this.addPageToDOMAndRenderJQM();
+        this.enhanceJQMComponentsAPI();
+    },
+
+    enhanceJQMComponentsAPI : function () {
+        $.mobile.changePage("#" + this.id, {
+            transition : 'none',
+            changeHash : false,
+            role : this.role
         });
-        event.preventDefault();
     },
 
-    onSignUp: function(event) {
+    addPageToDOMAndRenderJQM : function () {
+        $("body").append($(this.el));
+        $("#" + this.id).page();
+    },
+
+    cleanupPossiblePageDuplicationInDOM : function () {
+        var $previousEl = $("#" + this.id);
+        var isAlreadyInDom = $previousEl.length >= 0;
+        if (isAlreadyInDom) {
+            // used because from-to page switch
+            // needs events at that point of time
+            // have in mind, that detach will not remove handlers
+            $previousEl.detach();
+        }
+    }
+});
+
+SignUpView = BasicView.extend({
+    id: "signup",
+    templateId: "signup-template",
+
+    events: {
+        'click #signup-button': 'createOnClick',
+    },
+
+    createOnClick: function(event){
+        console.log('click create');
         var name = $('#signup-name').val()
         var email = $('#signup-email').val()
         var password = $('#signup-password').val()
@@ -52,26 +82,106 @@ var app = {
         user.set("email", email);
         user.set("name", name);
         user.signUp(null, {
-          success: function(user) {
-            // TODO show a message for successful signup before navigate
-            window.location.href = '#login';
-          },
-          error: function(user, error) {
-            // TODO: Show the error message somewhere and let the user try again.
-            console.log("Error: " + error.code + " " + error.message);
-          }
+            success: function(user) {
+                // TODO show a message for successful signup before navigate
+                Backbone.history.navigate('', true);
+            },
+            error: function(user, error) {
+                // TODO: Show the error message somewhere and let the user try again.
+                console.log("Error: " + error.code + " " + error.message);
+            }
         });
-        event.preventDefault();
     },
 
-    onHome: function(){
-        var list = document.getElementById("transaction-home");
-        list.innerHTML = tmpl("item_tmpl", {});
-        $('#transaction-home').trigger("create");
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
+    getSpecificTemplateValues: function(){
+        return {};
     }
-};
+});
+
+HomeView = BasicView.extend({
+    id: "home",
+    templateId: "home-template",
+
+    getSpecificTemplateValues: function(){
+        return {};
+    }
+});
+
+DetailView = BasicView.extend({
+    id: "detail",
+    templateId: "detail-template",
+
+    getSpecificTemplateValues: function(){
+        return {};
+    }
+});
+
+SignInView = BasicView.extend({
+    id: "signin",
+    templateId: "signin-template",
+
+    events: {
+        'click #signin-button': 'loginOnClick',
+    },
+
+    loginOnClick: function(event){
+        console.log('click login');
+        var username = $('#signin-username').val()
+        var password = $('#signin-password').val()
+
+        Parse.User.logIn(username, password, {
+            success: function(user) {
+                // Do stuff after successful login.
+                Backbone.history.navigate('home', true);
+            },
+            error: function(user, error) {
+                // The login failed. Check error to see why.
+                console.log("Error: " + error.code + " " + error.message);
+            }
+        });
+    },
+
+    getSpecificTemplateValues: function(){
+        return {};
+    }
+});
+
+var AppRouter = Backbone.Router.extend({
+    routes: {
+        "": "signin",
+        "signup": "signup",
+        "home": "home",
+        "detail": "detail",
+    },
+
+    signup: function() {
+        console.log('signup click');
+        new SignUpView();
+    },
+
+    signin: function() {
+        console.log('signin click');
+        new SignInView();
+    },
+
+    home: function() {
+        console.log('home click');
+        new HomeView();
+    },
+
+    detail: function() {
+        console.log('detail click');
+        new DetailView();
+    },
+});
+
+$(document).ready(function () {
+    console.log('document ready');
+    document.addEventListener('deviceready', function (){
+        console.log('device ready');
+        Parse.initialize("Va9dcafjWMkGn0PYMK7azpOsHANRD3Nk9AO9xq2e", "49bOHrbFxuQK6RZKB5QLgzuiBDKFYsUrqPiHmmQt");
+
+        new AppRouter();
+        Backbone.history.start({pushState: false});
+    }, false);
+});
